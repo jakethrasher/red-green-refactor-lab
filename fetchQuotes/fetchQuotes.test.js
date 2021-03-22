32994b55b1c1b669286aedd5efdc16d7e92c43cd
Jake Thrasher <mjakethrasher@gmail.com>
2021-03-22 16:59:28 -0700

    delete console.log

--- fetchQuotes/fetchQuotes.test.js
+++ fetchQuotes/fetchQuotes.test.js
@@ -1,15 +1,10 @@
 const fetchQuotes = require('./fetchQuotes');
 
 describe('fetchQuotes function', () => {
+    
   it('should fetch data and return a formatted response', async() => {
     const data = await fetchQuotes();
 
-    // const expectation = {
-    //   name: 'Bender',
-    //   text: 'Bite my shiny metal ass.',
-    //   image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
-    // }; 
-  
     expect(data).toEqual(expect.objectContaining({
       name: expect.any(String),
       text: expect.any(String),
